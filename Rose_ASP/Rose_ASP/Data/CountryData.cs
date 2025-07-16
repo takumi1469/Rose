c7fc87a2c45da1054db59b878397e5adfac7e348
Takumi Saito <takumi1469@gmail.com>
2025-07-16 21:37:33 +0200

    Before making branch

--- Rose_ASP/Rose_ASP/Data/CountryData.cs
+++ Rose_ASP/Rose_ASP/Data/CountryData.cs
@@ -2,7 +2,7 @@
 {
     public class CountryData
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public string CountryName { get; set; } = string.Empty;
         public string DataType { get; set; } = string.Empty; // "GDP", "Population"
         public int Year { get; set; }
